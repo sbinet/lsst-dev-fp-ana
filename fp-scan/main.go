f815cf06efa63efc2050bed93061c6b7c309f4a3
Sebastien Binet <binet@cern.ch>
2014-07-08 17:34:30 +0200

    fp-scan: first parse args, then print banner

--- fp-scan/main.go
+++ fp-scan/main.go
@@ -16,10 +16,10 @@ var (
 )
 
 func main() {
-	fmt.Printf("=== %s ===\n", filepath.Base(os.Args[0]))
 
 	flag.Parse()
 
+	fmt.Printf("=== %s ===\n", filepath.Base(os.Args[0]))
 	rc := run()
 
 	os.Exit(rc)
